e19604c5614ca6e946f96d7e342a463ab7bec0ce
unknown <denniscustiuc0@gmail.com>
2025-04-07 10:04:20 +0100

    Added exam notes

--- unisystem/src/pages/Home.tsx
+++ unisystem/src/pages/Home.tsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 function HomePage () {
-    return <p>You are home</p>
+    const [selectedOption, setSelectedOption] = useState('');
+
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value);
+    console.log('Selected:', event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="options">Choose an option:</label>
+      <select id="options" value={selectedOption} onChange={handleChange}>
+        <option value="">--Please choose an option--</option>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </select>
+
+      {selectedOption && <p>You selected: {selectedOption}</p>}
+    </div>
+  );
 }
 
 export default HomePage;
